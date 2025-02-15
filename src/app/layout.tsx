d61049a5a9c353668e0493d9b432110e4eb4e78c
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-16 00:20:39 +0500

    Changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type React from "react"
-import "@/styles/globals.css"
+import "./globals.css"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
 
