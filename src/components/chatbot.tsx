8220b26a3a50dea5e9f885f7eaf1ae6d68894297
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-17 11:36:00 +0500

    ChatBot

---
+++ src/components/chatbot.tsx
@@ -0,0 +1,12 @@
+import useKommunicate from "../hooks/useKommunicate";
+import { useEffect } from "react";
+
+const ChatBot: React.FC = () => {
+    useEffect(() => {
+        useKommunicate();
+    }, []);
+
+    return null;
+};
+
+export default ChatBot;
