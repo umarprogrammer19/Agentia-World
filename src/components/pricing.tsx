1abdeaf583aaa64b14eafe43fe38dc99d3813297
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-18 02:00:02 +0500

    Pricing

--- src/components/pricing.tsx
+++ src/components/pricing.tsx
@@ -43,8 +43,8 @@ const plans = [
 
 export function Pricing() {
     return (
-        <section id="pricing" className="py-24 relative overflow-hidden bg-gradient-to-r from-black via-purple-900/20 to-black md:bg-transparent">
-            <div className="absolute inset-0">
+        <section id="pricing" className="py-24 relative overflow-hidden">
+            <div className="hidden md:block absolute inset-0">
                 <div className="absolute inset-0 bg-[radial-gradient(circle_500px_at_50%_50%,#3B0764,transparent)]" />
                 <div className="absolute inset-0 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:14px_24px]" />
             </div>
